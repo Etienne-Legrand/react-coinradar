f2af44109c97a314c0d83d9114e6ea0e1a2bed14
Etienne Legrand <etiennewazer@gmail.com>
2025-02-16 21:19:42 +0100

    Ajout d'un fichier manifest.json et d'icônes pour l'extension CoinRadar, intégration de vite-plugin-static-copy pour la gestion des fichiers statiques

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+        {
+          src: "public/icons/*",
+          dest: "icons",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
