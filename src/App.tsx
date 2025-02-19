3c49118dd856d2f257109ce5b21473f1ecd6a0e4
Etienne Legrand <etiennewazer@gmail.com>
2025-02-19 11:31:33 +0100

    Amélioration du style de la scrollbar et correction de la taille de la popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { CoinList } from "./components/CoinList";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-white font-sans transition-colors duration-200 dark:bg-gray-900">
-      <div className="container mx-auto max-w-4xl">
-        <CoinList />
-      </div>
-    </div>
-  );
+  return <CoinList />;
 }
 
 export default App;
